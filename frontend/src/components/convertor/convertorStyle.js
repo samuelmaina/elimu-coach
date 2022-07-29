067d073a0a8089b14646314e17446f04460f5f74
samuelmayna <samuelmayna@gmail.com>
2022-07-29 20:08:38 +0300

    Styled the Convetor components using the repreated styles and the static sytles in css

---
+++ frontend/src/components/convertor/convertorStyle.js
@@ -0,0 +1,48 @@
+export const form = {
+  backgroundColor: "3a86ff",
+  height: "50vh",
+  marginLeft: "10%",
+  paddingTop: "2rem",
+  display: "flex",
+  width: "85%",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "solid 1px blue",
+  padding: "2%",
+};
+
+export const main = {
+  display: "flex",
+  alignItems: "center",
+  width: "100%",
+};
+
+export const numberSystem = {
+  display: "flex",
+  flexDirection: "column",
+  padding: "2rem",
+  width: "100%",
+};
+export const bases = {
+  display: "flex",
+  justifyContent: "space-around",
+};
+
+export const content = {
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+};
+
+export const numberContainer = {
+  width: "60%",
+  marginLeft: "20%",
+};
+
+export const explanation = {
+  width: "50%",
+  marginLeft: "20%",
+  fontSize: "15px",
+  backgroundColor: "#f8f8f8",
+};
